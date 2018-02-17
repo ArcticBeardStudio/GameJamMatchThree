8034f41247ca96fc486c783d9cc022d65a79cbb8
unknown <melsom.henrik@gmail.com>
2018-02-17 13:23:51 +0100

    fixed rename error with changeaction and changeinfo

---
+++ Assets/Scripts/ChangeStack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ChangeStack<T> where T : ChangeAction
+{
+    public Action<List<T>> resolveCallback;
+
+    public int length { get { return stack == null ? 0 : stack.Count; } }
+
+    bool isOpen;
+    List<T> stack;
+    List<T> history;
+
+    public ChangeStack(Action<List<T>> resolveCallback)
+    {
+        this.resolveCallback = resolveCallback;
+
+        isOpen = false;
+    }
+
+    public void Begin()
+    {
+        isOpen = true;
+        stack = new List<T>();
+        history = new List<T>();
+    }
+
+    public void Add(T change)
+    {
+        if (!isOpen)
+        {
+            Debug.LogWarning("You need to call Begin() on the stack before adding changes. ");
+            return;
+        }
+        stack.Add(change);
+    }
+
+    public void End()
+    {
+        isOpen = false;
+        foreach (T change in stack.ToArray())
+        {
+            change.DoChange(CheckResolved);
+        }
+    }
+
+    void CheckResolved() 
+    {
+        foreach (T change in stack.ToArray())
+        {
+            if (change.isComplete) 
+            {
+                history.Add(change);
+                stack.Remove(change);
+            }
+        }
+        if (stack.Count <= 0)
+        {
+            resolveCallback(history);
+        }
+    }
+}
