44c76ec91efcb7cb01740d81c13cc196aadca212
unknown <melsom.henrik@gmail.com>
2018-02-17 15:15:00 +0100

    can swap empty tiles

--- Assets/Scripts/ChangeAction.cs
+++ Assets/Scripts/ChangeAction.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Base class for handling changes on the board.
+/// See <cref="CreateAction">, <cref="SwapAction"> or <cref="RemoveAction"> for implementation examples.
+/// </summary>
 public class ChangeAction
 {
     public Board board;
     public bool isComplete;
 
+    /// <summary>
+    /// Initializes a new change with default values
+    /// </summary>
+    /// <param name="board">Which board this change affects</param>
     public ChangeAction(Board board)
     {
         this.board = board;
         isComplete = false;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="callback"></param>
     public void DoChange(System.Action callback)
     {
         Debug.Assert(board, "Board is null");
