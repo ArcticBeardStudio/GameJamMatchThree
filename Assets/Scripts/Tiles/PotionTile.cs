34d135f50afff3ee48f86061ca2dcb9932e41006
pvallsten <petval-0@hotmail.com>
2018-02-18 18:02:56 +0100

    Added potiontile

---
+++ Assets/Scripts/Tiles/PotionTile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionTile : Tile
+{
+    public int healAmount = 1;
+
+    public override void DestroyTile()
+    {
+        var player = GameManager.GetCurrentPlayer().GetComponent<Player>();
+        if (player != null && player.isActiveAndEnabled)
+        {
+            player.ModifyHealth(healAmount);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
